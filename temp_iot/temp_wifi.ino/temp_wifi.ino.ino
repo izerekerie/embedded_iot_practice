6311ba5a739b558e5eff79336ecbe7d0764f7787
izereuwonkunda <izereuwonkundakelia@gmail.com>
2022-07-11 21:26:51 +0200

    buzzer

--- temp_iot/temp_wifi.ino/temp_wifi.ino.ino
+++ temp_iot/temp_wifi.ino/temp_wifi.ino.ino
@@ -26,34 +26,46 @@ void loop(){
     float vcc = 5.0;
     float voltage = rawData * (vcc / 1024.0);
     float temperature = voltage * 100;
-
+//    float vcc = 3.3;
+//    float millivolts = rawData * (vcc / 1024.0);
+//    float temperature = millivolts * 100;
+//    float offset = 3.0;
+//    temperature = temperature-offset;
       if (temperature == 35){
     digitalWrite(greenPin,LOW);
     digitalWrite(redPin,HIGH);
     digitalWrite(buzzer.HIGH);
     delay(500);
     digitalWrite(buzzer.HIGH);
-    lcd.setCursor(0,1);   
-    lcd.print(temperature);
+   String host = "insecure.benax.rw";
+  String path = "/iot/";
+  int port = 80;
+ // String request = "POST /insecure.benax.rw/iot/?device=RCA0125BGE&temperature="+(String)temperature+" HTTP/1.1";
+  String mData="";
+  String device = "kerie";
+  mData = "device="+device+"&distance="+(String)temperatire;
+  sendData(port, host,path,mData); 
       }
      else {
     digitalWrite(greenPin,HIGH);
     digitalWrite(redPin,LOW);
     digitalWrite(buzzer.LOW);  
        }
-  String host = "insecure.benax.rw";
-  String path = "/iot/";
-  int port = 8000;
-  String request = "POST /insecure.benax.rw/iot/?device=RCA0125BGE&temperature="+(String)temperature+" HTTP/1.1";
-  wifiClient.connect(host, port);
-  wifiClient.println(request);
-  wifiClient.println("Host: "+host);
+    lcd.clear();
+    lcd.setCursor(0,1);   
+    lcd.print(temperature);
+    delay(1000);
+}
+
+void sendData(const int httpPort, const char* host,const char* filepath , String data){
+  wifiClient.connect(host, httpPort); 
+  wifiClient.println("POST "+(String)filepath+" HTTP/1.1");
+  wifiClient.println("Host: " + (String)host);
   wifiClient.println("User-Agent: ESP8266/1.0");
-  wifiClient.println("C: ESP8266/1.0");
+  wifiClient.println("Content-Type: application/x-www-form-urlencoded");
+  wifiClient.println("Content-Length: " +(String)data.length());
   wifiClient.println();
-  Serial.print("Temperature: ");
-  Serial.print(temperature);
-  Serial.println("*C");
-  Serial.println("Response: "+wifiClient.readStringUntil('\n'));
-  delay(1000);
+  wifiClient.print(data);
+  Serial.println("Response: " + wifiClient.readStringUntil('\n'));
+
 }
